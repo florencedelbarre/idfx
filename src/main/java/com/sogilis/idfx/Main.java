d046e1f43577f361942f491d564fb0992510d897
Corentin Marciau <corentin@sogilis.com>
2018-12-12 14:55:47 +0100

    [DEV] Simple login screen

--- src/main/java/com/sogilis/idfx/Main.java
+++ src/main/java/com/sogilis/idfx/Main.java
@@ -1,7 +1,35 @@
 package com.sogilis.idfx;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Bonjour !");
+        Application.launch(Main.class, args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Première application");
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/Main.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    protected void onConnectionButton(ActionEvent event) {
+        System.out.println("Coucou !");
+    }
+
 }
