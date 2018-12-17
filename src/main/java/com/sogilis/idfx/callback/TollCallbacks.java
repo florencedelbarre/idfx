e55e5e76637195ac624636533ffb38a1b813fd52
Corentin Marciau <corentin@sogilis.com>
2018-12-17 14:48:14 +0100

    [DEV] Add back button to toll scene

--- src/main/java/com/sogilis/idfx/callback/TollCallbacks.java
+++ src/main/java/com/sogilis/idfx/callback/TollCallbacks.java
@@ -1,12 +1,17 @@
 package com.sogilis.idfx.callback;
 
 import com.sogilis.idfx.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
 import java.util.Locale;
 
 public class TollCallbacks {
+
+    @FXML private void onBackPressed(ActionEvent event) {
+        Main.primaryStage.setScene(Main.loginScene);
+    }
 
     @FXML public void setFrench() throws IOException {
         Main.rebuildAllScenes(Locale.FRANCE);
